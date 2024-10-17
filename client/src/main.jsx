98928bfece6aff3fd2948cb78fdb96ec18198dd7
Grant Cawley <grantlcawley@gmail.com>
2024-10-17 03:50:42 -0400

    fix: remove initial tutorial pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,6 @@ import {
 import App from "./App";
 import LandingPage from "./components/LandingPage";
 import RecipeDetails from "./components/RecipeDetails";
-// import RecipeList from "./components/RecipeList";
-// import Recipe from "./components/Recipe";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -31,27 +29,7 @@ const router = createBrowserRouter([
         element: <RecipeDetails />,
       },
     ],
-  },
-  // {
-  //   path: "/edit/:id",
-  //   element: <App />,
-  //   children: [
-  //     {
-  //       path: "/edit/:id",
-  //       element: <Recipe />,
-  //     },
-  //   ],
-  // },
-  // {
-  //   path: "/create",
-  //   element: <App />,
-  //   children: [
-  //     {
-  //       path: "/create",
-  //       element: <Recipe />,
-  //     },
-  //   ],
-  // },
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
