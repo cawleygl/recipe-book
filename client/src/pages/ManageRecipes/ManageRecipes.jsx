4802f652d279f7ab2ee788ccaa7cf2f66e67fcc7
Grant Cawley <grantlcawley@gmail.com>
2024-11-19 14:57:07 -0500

    recipe view page

---
+++ client/src/pages/ManageRecipes/ManageRecipes.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getRecipes } from "../../utils/api";
+import { ListGroup } from "react-bootstrap";
+
+export default function CreateRecipe() {
+  const navigate = useNavigate();
+  // All Recipes List
+  const [recipeList, setRecipeList] = useState([]);
+
+  async function handleRecipeFetch() {
+    setRecipeList(await getRecipes());
+  }
+
+  useEffect(() => {
+    handleRecipeFetch();
+  }, []);
+
+  return (
+    <>
+      <h1>Recipes</h1>
+      <ListGroup>
+        {recipeList &&
+          recipeList.map((recipe, index) => {
+            return (
+              <ListGroup.Item
+                key={index}
+                action
+                variant="primary"
+                onClick={() => navigate(`/recipes/${recipe._id}`)}
+              >
+                {recipe.dishName}&nbsp;-&nbsp;{recipe.description.trim()}
+              </ListGroup.Item>
+            );
+          })}
+      </ListGroup>
+    </>
+  );
+}
