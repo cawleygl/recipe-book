6f51726f5cef4d8b3549918b8d8eb2bdb2804b52
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-03-31 19:30:54 -0400

    fix: require path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import recipeRoutes from "./routes/recipeRoutes.js";
 import ingredientRoutes from "./routes/ingredientRoutes.js";
+const path = require('path');
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -12,8 +13,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
-
+}
 
 app.use(cors());
 app.use(express.json());
