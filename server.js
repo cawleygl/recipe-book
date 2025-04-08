e643e01c387cd484216da1a6e578bdd0ef6361e9
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-04-08 10:01:48 -0400

    fix: client directory

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static('client/build'));
+    app.use(express.static('client/dist'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
 }
 
 app.use(cors());
