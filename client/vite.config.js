996520697a68e3fac2e80c2c6b7697e36b19491e
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-04-14 18:31:46 -0400

    fix: allow host

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: process.env.PORT || 10000
+    port: process.env.PORT || 10000,
+    allowedHosts: "recipe-book-xski.onrender.com"
   }
 });
