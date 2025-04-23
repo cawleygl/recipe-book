535ffb804dfdf24763b8212c8331996af827fc94
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-04-23 15:14:33 -0400

    fix: file location

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/dist'));
+// if (process.env.NODE_ENV === 'production') {
+//     //*Set static folder up in production
+//     app.use(express.static('client/dist'));
     
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
-}
+//     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
+// }
 
 app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
